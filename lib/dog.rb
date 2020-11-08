fe7e8271f5369d8bd478835b7d4c1afaefbdd9c5
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-08 00:57:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,7 +24,10 @@ class Dog
       sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      DB[:conn].execute("SELECT * FROM dogs WHERE id = @id")
+      DB[:conn].execute("SELECT * FROM dogs WHERE id = @id"). do |row|
+        hash = {}
+        binding.pry
+      end
     # binding.pry
     end
   end
