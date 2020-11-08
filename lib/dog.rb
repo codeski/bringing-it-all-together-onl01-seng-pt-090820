4196720f7be5d40089594488c0a084358212d999
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-08 02:32:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,7 @@ class Dog
       sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      arrays = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", @id)
+      sql = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", @id)
       binding.pry
       # pokemon_data = arrays[0]
       # binding.pry
