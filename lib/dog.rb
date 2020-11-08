48c5a9e975813d719636e4ad647858cf1fade550
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-08 02:13:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
       sql = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", @id)
-      sql
+      sql = sql_data[0]
       
     end
   end
